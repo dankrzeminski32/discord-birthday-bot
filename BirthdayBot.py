71d718ecce653ac43b13fb912532aee00108de19
dankrzeminski32 <dankrzeminski32@gmail.com>
2022-08-26 17:30:20 -0500

    created boilerplate

---
+++ BirthdayBot.py
@@ -0,0 +1,21 @@
+from config import DiscordBotToken
+import discord
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+client.run(DiscordBotToken)
