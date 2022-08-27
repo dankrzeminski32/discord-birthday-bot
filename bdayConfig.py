c74a6185b807ec78902381f487348cee5ed8a93e
Ethan <ethankvachkoff@gmail.com>
2022-08-27 12:15:23 -0500

    new pips + added more loops to bdayfinder function

--- bdayConfig.py
+++ bdayConfig.py
@@ -1,11 +1,13 @@
 import csv
 import discord
-from dateutil.parser import parse
+import pandas as pd
+from datetime import datetime
 
 class bdayConfig:
 
     def bdayFinder():
         rows = []
+        count=0
 
         with open("DiscordBirthdays.csv", 'r') as file:
             csvreader = csv.reader(file)
@@ -16,15 +18,20 @@ class bdayConfig:
         for i in range(len(rows)):
                 for j in range(len(rows[i])):
                         print(rows[i][j])
+                        
+        #Change so it checks for todays date instead of specified date
+        for row in csvreader:
+            if row[0] !="Birthday":    #might need to change "birthday" to "name"           
+                date = datetime.datetime.strptime (row [0],"%d/%m/%y")     
+                if date < datetime.datetime.strptime ("2014-09-26 00:00:00", "%d/%m/%y")and date > datetime.datetime.strptime ("2014-09-25 00:00:00", "%d/%m/%y"):
+                    count = count+1
 
-        for myline in csvreader:
-            header=myline
-            break
+    def isDateValid(date, pattern = "%d/%m/%y"):
+        try:
+            datetime.strptime(date, pattern)
+            return True
+        except ValueError:
+            return False
 
-        for myline in csvreader:
-            for myColIndex in range(len(myline)):
-                if (parse(myline[myColIndex])):
-                    print("column = {0}".format(myColIndex))
-                    
     bdayFinder()
     
