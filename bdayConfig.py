f7c57dc04fb696d11956ad4ba2fde91e75233b50
Ethan <ethankvachkoff@gmail.com>
2022-08-27 16:46:56 -0500

    Added date check

--- bdayConfig.py
+++ bdayConfig.py
@@ -1,8 +1,8 @@
 import csv
-from tkinter.tix import COLUMN
 import discord
 import pandas as pd
 from datetime import datetime
+from datetime import date
 
 class bdayConfig:
     data = pd.read_csv("DiscordBirthdays.csv")
@@ -10,12 +10,12 @@ class bdayConfig:
     def bdayFinder():
         rows = []
         count=0  
-
         with open("DiscordBirthdays.csv", 'r') as file:
             csvreader = csv.reader(file)
             header = next(csvreader)
             for row in csvreader:
                 rows.append(row)
+
 
     def isDateValid(date, pattern = "%m/%d/%y"):
         try:
@@ -32,5 +32,10 @@ class bdayConfig:
             print(i)
         else:
             print("This date is invalid")
+
+
+    today = date.today()
+    today = today.strftime("%m/%d")
+    print("Today's date:", today)
     bdayFinder()
     
