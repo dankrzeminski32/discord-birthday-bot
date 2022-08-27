104b049266afd68c3b4ee9ebc564191779b21540
dankrzeminski32 <dankrzeminski32@gmail.com>
2022-08-27 12:44:31 -0500

    establish primary run file/main event loop

--- BirthdayBot/BirthdayBot.py
+++ BirthdayBot/BirthdayBot.py
@@ -24,38 +24,3 @@ else:
             myFile =csv.writer(file)
 
             myFile.writerow(["DiscordName","Birthday"])
-
-@client.event
-async def on_ready():
-    print(f'We have logged in as {client.user}')
-
-@client.command()
-async def test(ctx):
-    await ctx.send("Hello World!")
-
-@client.command()
-async def bday(ctx):
-    embed = discord.Embed(
-        title = "Please enter your Birthday (Ex:07/11/01)",
-        description = "This will store your birthday in our database",
-        color = discord.Color.blue()
-    )
-    await ctx.send(embed=embed)
-
-    def check(msg):
-        return msg.author == ctx.author and msg.channel == ctx.channel and msg.content.startswith("0")
-
-    msg = await client.wait_for('message', check=check)
-    if msg.content.startswith("0"):
-        await ctx.send("Your birthday has been stored in our database!")
-
-    await ctx.send(msg.content)    
-    user = ctx.author
-    userBirthday = msg.content
-    await ctx.send(ctx.author)
-    
-    with open('DiscordBirthdays.csv', "a", newline="") as file:
-        myFile = csv.writer(file)
-        myFile.writerow([user, userBirthday])
-
-client.run(DiscordBotToken)
--- run.py
+++ run.py
@@ -1,11 +1,29 @@
+import asyncio
 import discord
-from config import DiscordBotToken
 from discord.ext import commands
+from config import DISCORD_BOT_TOKEN
+import sys
 
-
+# Create permission intents, state what our bot should be able to do
 intents = discord.Intents.default()
 intents.message_content = True
 
-
 bot = commands.Bot(command_prefix = ".", intents = intents)
-bot.run(DiscordBotToken)
+
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+
+extensions = ['BirthdayBot.commands.Register']
+
+async def load_extensions():
+    extensions = ['BirthdayBot.commands.Register']
+    for filename in extensions:
+            await bot.load_extension(filename)
+
+async def main():
+    async with bot:
+        await load_extensions()
+        await bot.start(DISCORD_BOT_TOKEN)
+
+asyncio.run(main())
