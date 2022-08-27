bf87cf25e42f677b7b589934d4e6718226152ed7
Ethan <ethankvachkoff@gmail.com>
2022-08-27 16:34:28 -0500

    Added a single column to a list to iterate from

--- bdayConfig.py
+++ bdayConfig.py
@@ -1,13 +1,15 @@
 import csv
+from tkinter.tix import COLUMN
 import discord
 import pandas as pd
 from datetime import datetime
 
 class bdayConfig:
+    data = pd.read_csv("DiscordBirthdays.csv")
 
     def bdayFinder():
         rows = []
-        count=0
+        count=0  
 
         with open("DiscordBirthdays.csv", 'r') as file:
             csvreader = csv.reader(file)
@@ -15,23 +17,20 @@ class bdayConfig:
             for row in csvreader:
                 rows.append(row)
 
-        for i in range(len(rows)):
-                for j in range(len(rows[i])):
-                        print(rows[i][j])
-                        
-        #Change so it checks for todays date instead of specified date
-        for row in csvreader:
-            if row[0] !="Birthday":    #might need to change "birthday" to "name"           
-                date = datetime.datetime.strptime (row [0],"%d/%m/%y")     
-                if date < datetime.datetime.strptime ("2014-09-26 00:00:00", "%d/%m/%y")and date > datetime.datetime.strptime ("2014-09-25 00:00:00", "%d/%m/%y"):
-                    count = count+1
-
-    def isDateValid(date, pattern = "%d/%m/%y"):
+    def isDateValid(date, pattern = "%m/%d/%y"):
         try:
             datetime.strptime(date, pattern)
             return True
         except ValueError:
             return False
 
+    data.columns = ["Name", "Birthday"]
+    rawlist = list(data.Birthday)
+    print(rawlist)
+    for i in rawlist:
+        if isDateValid(i,"%m/%d/%y"):
+            print(i)
+        else:
+            print("This date is invalid")
     bdayFinder()
     
