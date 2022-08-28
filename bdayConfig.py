d91be6f989a57e6bf4262dc7d3bd15f5bd33e9a3
Ethan <ethankvachkoff@gmail.com>
2022-08-28 14:16:58 -0500

    Wroking name and birthday getter

--- bdayConfig.py
+++ bdayConfig.py
@@ -4,17 +4,52 @@ import pandas as pd
 from datetime import datetime
 from datetime import date
 
-class bdayConfig:
+class BdayConfig:
+    """Has methods that returns certain data pretaining the .csv data file. Thus, having access to names and birthdays."""
 
-    def isDateValid(date, pattern = "%m/%d/%y"):
+    def __init__(self):
+        self.csvfileName = "DiscordBirthdays.csv"
+
+        self.data = pd.read_csv(self.csvfileName)
+        self.data.columns = ["DiscordName", "Birthday"]
+        self.dateList = list(self.data.Birthday)
+        self.nameList = list(self.data.DiscordName)
+
+
+    def isDateValid(self, date, pattern = "%m/%d"):
         try:
-            datetime.strptime(date, pattern)
+            datetime.strptime(date[:-3], pattern)#[:-3] Takes off the last 3 characters of the date
             return True
-        except ValueError:
+        except ValueError as e:
+            print(e)
             return False
 
-    def bdayCheck():
+    def getBirthdays(self):
         todaysBirthdays = []
+
+        today = date.today()
+        today = today.strftime("%m/%d/%y")
+        
+        print(self.dateList)
+        print(self.nameList)
+
+        for i in self.dateList:
+            if i == today and self.isDateValid(str(i), "%m/%d"):
+                todaysBirthdays += [i]
+                birthday = True
+            else:
+                birthday = False
+
+        print(birthday)
+        print(todaysBirthdays)
+        return todaysBirthdays
+
+    def getDiscordNames(self):
+        todaysBirthdays = []
+        birthdayDiscordNames = []
+
+        count = 0
+
         today = date.today()
         today = today.strftime("%m/%d/%y")
         #print("Today's date:", today)
@@ -23,23 +58,40 @@ class bdayConfig:
         data.columns = ["DiscordName", "Birthday"]
         dateList = list(data.Birthday)
         nameList = list(data.DiscordName)
-        print(dateList)
-        print(nameList)
+
+        for row in data:
+            print(row)
 
         for i in dateList:
             if i == today:
                 todaysBirthdays += [i]
+                birthdayDiscordNames += [i]
                 birthday = True
             else:
+                
                 birthday = False
-            #if isDateValid(i,"%m/%d/%y"):
-                #print(i)
-            #else:
-                #print("This date is invalid")
-        print(birthday)
-        print(todaysBirthdays)
+
+        print(birthdayDiscordNames)
+    
+    def testFunction(self):
+        today = date.today()
+        today = today.strftime("%m/%d/%y")
+
+        with open(self.csvfileName) as f:
+            reader = csv.reader(f)
+            next(reader, None)
+            for row in reader:
+                if row[1] == today and self.isDateValid(str(row[1]), "%m/%d"):
+                    print(row)
+        
 
 
 
-    bdayCheck()
+bdayConfigObject = BdayConfig()
+
+
+
+#bdayConfigObject.getBirthdays()
+#bdayConfigObject.getDiscordNames()
+bdayConfigObject.testFunction()
     
