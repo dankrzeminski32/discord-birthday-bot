a656adffd27b999abb1ee1bd8f0017505ccc5c83
dankrzeminski32 <dankrzeminski32@gmail.com>
2022-08-31 23:13:57 -0500

    buttons are showing up

--- BirthdayBot/commands/register.py
+++ BirthdayBot/commands/register.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext import commands
 import csv
+from discord.ui import Button, View
 
 class Registration(commands.Cog):
     """Class Dedicated to housing all commands related to registration"""
@@ -18,9 +19,15 @@ class Registration(commands.Cog):
 
         msg = await self.bot.wait_for('message', check=check)
         await ctx.send("{}, Your birthday ({}) has been stored in our database!".format(msg.author,msg.content))
+        await self.sendConfirmationMessage(ctx)
         self.writeUserToCSV(username = msg.author,birthday = msg.content)
 
     """ ---- HELPERS ---- """
+    async def sendConfirmationMessage(self, ctx):
+        view = RegistrationButtons()
+        await ctx.send("Is this correct?", view=view)
+    
+    
     async def sendRegistrationMessage(self, ctx):
         embed = discord.Embed(
             title = "Please enter your Birthday (Ex:07/11/01)",
@@ -37,3 +44,18 @@ class Registration(commands.Cog):
 
 async def setup(bot):
     await bot.add_cog(Registration(bot))
+
+
+class RegistrationButtons(discord.ui.View):
+    def __init__(self, *, timeout=180):
+        super().__init__(timeout=timeout)  
+        
+    @discord.ui.button(label="Yes!",style=discord.ButtonStyle.green) # or .success
+    async def green_button(self,button:discord.ui.Button,interaction:discord.Interaction):
+        button.disabled=True
+        await interaction.response.edit_message(view=self)
+        
+    @discord.ui.button(label="No!",style=discord.ButtonStyle.red) # or .danger
+    async def red_button(self,button:discord.ui.Button,interaction:discord.Interaction):
+        button.disabled=True
+        await interaction.response.edit_message(view=self)
