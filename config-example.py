d4cf468dffedd1773b5637817e82a0fd6efd93f5
dankrzeminski32 <dankrzeminski32@gmail.com>
2022-10-06 21:38:22 -0500

    added config example

---
+++ config-example.py
@@ -0,0 +1,11 @@
+# DISCORD
+DISCORD_BOT_TOKEN: str = "TOKEN"
+
+# POSTGRES
+POSTGRES_USER: str = "DB-USERNAME"
+POSTGRES_PASS: str = "DB-PASS"
+POSTGRES_DB: str = "birthdaybotdb"
+POSTGRES_HOST: str = "127.0.0.1"
+POSTGRES_PORT: str = "5432"
+DATABASE_URI = "postgresql+psycopg2://USER:PASS@localhost:5432/birthdaybotdb"
+PATH_TO_BIRTHDAY_QUOTES: str = "BirthdayBot/Assets/BirthdayMessages.txt"
