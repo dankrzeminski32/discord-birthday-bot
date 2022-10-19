a8a5da25f1bf07a950e016cfc13adf236135ff1a
dankrzeminski32 <dankrzeminski32@gmail.com>
2022-10-19 07:42:15 -0500

    updated gitignore, remove test file, add assets

--- test.py
+++
@@ -1,7 +0,0 @@
-from datetime import datetime
-from pytz import timezone
-import pytz    
-# Current time in UTC
-now_utc = datetime.now(timezone('America/Chicago'))
-
-print(now_utc.day, now_utc.month)
